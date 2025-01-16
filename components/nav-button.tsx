9c571df5883fd40ad9610cb999b378c4b7af2a38
Luiz Pires <luizprsjr@gmail.com>
2025-01-16 01:35:36 -0300

    feat: create mobile header

--- components/nav-button.tsx
+++ components/nav-button.tsx
@@ -1,4 +1,5 @@
 import Link from "next/link";
+
 import { Button } from "./ui/button";
 import { cn } from "@/lib/utils";
 
@@ -21,7 +22,7 @@ export const NavButton = ({
       className={cn(
         "w-full lg:w-auto justify-between font-normal hover:bg-white/20 hover:text-white border-none focus-visible:ring-offset-0 focus-visible:ring-transparent outline-none text-white focus:bg-white/30 transition",
         isActive ? "bg-white/10 text-white" : "bg-transparent",
-      )}    
+      )}
     >
       <Link href={href}>
         {label}
